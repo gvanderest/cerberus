b44687740f8c0935ae895a41103a5fcc3f1f83f6
Guillaume VanderEst <gvanderest@gmail.com>
2024-01-12 16:16:19 -0800

    Some initial code to listen and extract username/password

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::net::{TcpListener, TcpStream};
+use std::{
+    io::{prelude::*, BufReader},
+    net::TcpListener,
+};
 
 struct Config {
     host: String,
@@ -15,12 +18,68 @@ fn main() -> std::io::Result<()> {
 
     println!("Server up and running at {host_port}..");
 
-    let mut streams = vec![];
-
     // accept connections and process them serially
     for stream in listener.incoming() {
-        streams.push(stream);
+        let stream = stream.unwrap();
+        let mut reader = BufReader::new(&stream);
         println!("GOT A NEW CONNECTION!!");
+
+        let mut packet_type: [u8; 6] = [0; 6];
+
+        loop {
+            let packet_type_check = reader.read_exact(&mut packet_type);
+
+            if packet_type_check.is_ok() {
+                println!("PACKET TYPE: {packet_type:?}");
+                if packet_type == [100, 0, 55, 0, 0, 0] {
+                    let mut username = [0; 24];
+                    reader.read_exact(&mut username)?;
+
+                    let mut password = [0; 24];
+                    reader.read_exact(&mut password)?;
+
+                    println!("RAW LOGIN CREDENTIALS, username={username:?}, password={password:?}");
+
+                    let username_index_of_null =
+                        username.iter().enumerate().find(|(_index, c)| **c == 0);
+                    let username = String::from_utf8(
+                        username
+                            .to_vec()
+                            .iter()
+                            .take(match username_index_of_null {
+                                Some((index, _char)) => index,
+                                _ => username.len(),
+                            })
+                            .copied()
+                            .collect(),
+                    )
+                    .unwrap();
+
+                    let password_index_of_null =
+                        password.iter().enumerate().find(|(_index, c)| **c == 0);
+                    let password = String::from_utf8(
+                        password
+                            .to_vec()
+                            .iter()
+                            .take(match password_index_of_null {
+                                Some((index, _char)) => index,
+                                _ => password.len(),
+                            })
+                            .copied()
+                            .collect(),
+                    )
+                    .unwrap();
+
+                    println!("LOGIN CREDENTIALS, username={username:?}, password={password:?}");
+                } else {
+                    println!("UNHANDLED PACKET TYPE: {:?}", packet_type);
+                    break;
+                }
+            } else {
+                println!("Connection lost!");
+                break;
+            }
+        }
     }
     Ok(())
 }
