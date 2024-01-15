697ff55169004a167ba7a4402437a93b9e611497
Guillaume VanderEst <gvanderest@gmail.com>
2024-01-15 10:46:11 -0800

    Update some comments and add notes

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
-use byteorder::{ByteOrder, LittleEndian};
-use std::{
-    io::{prelude::*, BufReader},
-    net::TcpListener,
-}; // 1.3.4
-
 use bufstream::BufStream;
+use byteorder::{ByteOrder, LittleEndian};
+use std::{io::prelude::*, net::TcpListener};
 
-struct Config {
+struct LoginServerConfig {
     host: String,
     port: u16,
 }
+// TODO: Spin up character server
+// TODO: When auth successful to login server, allow-list connection for a time period to character server for selection
+// TODO: On character selection, allow-list connection for map servers.. have servers maintain allowed IPs list, to identify hacking attempts and block
 
 // Login server packets
 const LOGIN_AUTH_ATTEMPT: u16 = 0x0064;
 
 fn main() -> std::io::Result<()> {
-    let config = Config {
+    let config = LoginServerConfig {
         host: "0.0.0.0".to_string(),
         port: 6900,
     };
@@ -40,6 +39,7 @@ fn main() -> std::io::Result<()> {
             if command_type_check.is_ok() {
                 let command_type = LittleEndian::read_u16(&raw_command_type);
                 println!("COMMAND TYPE: 0x{command_type:04x}");
+
                 if command_type == LOGIN_AUTH_ATTEMPT {
                     println!("This is a login attempt command");
                     // Padding, not sure what these represent yet
@@ -84,9 +84,9 @@ fn main() -> std::io::Result<()> {
                     )
                     .unwrap();
 
-                    // Padding, not sure.. always 0x00 suffix
-                    let mut padding: [u8; 1] = [0; 1];
-                    stream.read_exact(&mut padding)?;
+                    // Gender? 0x00 for male, 0x01 for female?
+                    let mut gender: [u8; 1] = [0; 1];
+                    stream.read_exact(&mut gender)?;
 
                     println!("LOGIN CREDENTIALS, username={username:?}, password={password:?}");
 
