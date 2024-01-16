7a71e84be41119d549e4a11df1da521cba7815e4
Guillaume VanderEst <gvanderest@gmail.com>
2024-01-16 11:22:39 -0800

    Split logic into some function calls

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use anyhow::Result;
 use bufstream::BufStream;
 use byteorder::{ByteOrder, LittleEndian};
 use std::{
     io::prelude::*,
-    net::{Shutdown, TcpListener},
+    net::{Shutdown, TcpListener, TcpStream},
 };
 
 struct LoginServerConfig {
@@ -16,7 +17,141 @@ struct LoginServerConfig {
 // Login server packets
 const LOGIN_AUTH_ATTEMPT: u16 = 0x0064;
 
-fn main() -> std::io::Result<()> {
+struct LoginAuthenticateData {
+    unknown_1: u32,
+    username: String,
+    password: String,
+    unknown_2: u8,
+}
+
+type Stream<'a> = BufStream<&'a mut TcpStream>;
+
+fn read_login_authenticate_data(stream: &mut Stream) -> Result<LoginAuthenticateData> {
+    // Padding, not sure what these represent yet
+    let mut raw_unknown_1: [u8; 4] = [0; 4];
+    stream.read_exact(&mut raw_unknown_1)?;
+    let unknown_1 = u32::from_le_bytes(raw_unknown_1);
+
+    let mut username = [0; 24];
+    stream.read_exact(&mut username)?;
+
+    let mut password = [0; 24];
+    stream.read_exact(&mut password)?;
+
+    println!("RAW LOGIN CREDENTIALS, username={username:?}, password={password:?}");
+
+    let username_index_of_null = username.iter().enumerate().find(|(_index, c)| **c == 0);
+    let username = String::from_utf8(
+        username
+            .to_vec()
+            .iter()
+            .take(match username_index_of_null {
+                Some((index, _char)) => index,
+                _ => username.len(),
+            })
+            .copied()
+            .collect(),
+    )
+    .unwrap();
+
+    let password_index_of_null = password.iter().enumerate().find(|(_index, c)| **c == 0);
+    let password = String::from_utf8(
+        password
+            .to_vec()
+            .iter()
+            .take(match password_index_of_null {
+                Some((index, _char)) => index,
+                _ => password.len(),
+            })
+            .copied()
+            .collect(),
+    )
+    .unwrap();
+
+    let mut raw_unknown_2: [u8; 1] = [0; 1];
+    stream.read_exact(&mut raw_unknown_2)?;
+
+    Ok(LoginAuthenticateData {
+        unknown_1,
+        username,
+        password,
+        unknown_2: raw_unknown_2[0],
+    })
+}
+
+fn write_login_invalid_login_error(stream: &mut Stream) -> Result<()> {
+    let bad_password: [u8; 30] = [
+        0xe0, 0x0a, 0x54, 0x14, 0x00, 0x00, 0x2b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x2d,
+        0x00, 0x00, 0x2d, 0x00, 0x00, 0x20, 0x00, 0x00, 0x3a, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    ];
+    stream.write_all(&bad_password)?;
+    stream.flush().unwrap();
+
+    Ok(())
+}
+
+fn write_login_authenticate_success(stream: &mut Stream) -> Result<()> {
+    let login_success_command: u16 = 0x0a4d;
+    let weird_bytes: u16 = 0x00a0;
+    let login_id1: u32 = 123456;
+    let account_id: u32 = 55555;
+    let login_id2: u32 = 0;
+    let unknown_bytes: [u8; 31] = [0; 31];
+
+    let mut raw_mnemonic: [u8; 16] = [0; 16];
+    for (ch_index, ch) in "TPZMgc02COiARyrU".chars().enumerate() {
+        raw_mnemonic[ch_index] = ch as u8;
+    }
+    let gender: u8 = 0;
+
+    let mut packet: Vec<u8> = vec![];
+    packet.append(&mut login_success_command.to_le_bytes().to_vec());
+    packet.append(&mut weird_bytes.to_le_bytes().to_vec());
+    packet.append(&mut login_id1.to_le_bytes().to_vec());
+    packet.append(&mut account_id.to_le_bytes().to_vec());
+    packet.append(&mut login_id2.to_le_bytes().to_vec());
+    packet.append(&mut unknown_bytes.to_vec());
+    packet.append(&mut raw_mnemonic.to_vec());
+    packet.append(&mut gender.to_le_bytes().to_vec());
+
+    println!("Raw packet: len={}, body={packet:?},", packet.len());
+
+    stream.write_all(&packet)?;
+    stream.flush()?;
+    Ok(())
+}
+
+fn write_login_character_servers_list(stream: &mut Stream) -> Result<()> {
+    let server_list_command_prefix: u16 = 0xf180;
+    let population: u16 = 123;
+    let ip_suffix: &[u8; 2] = &[1, 70];
+    let port: u16 = 4501;
+
+    let name = "Cerberus";
+    let mut raw_name: [u8; 20] = [0; 20];
+    for (ch_index, ch) in name.chars().enumerate() {
+        raw_name[ch_index] = ch as u8;
+    }
+
+    let padding_suffix: [u8; 4] = [0; 4];
+
+    let mut packet: Vec<u8> = vec![];
+    packet.append(&mut server_list_command_prefix.to_le_bytes().to_vec());
+    packet.append(&mut ip_suffix.to_vec());
+    packet.append(&mut port.to_le_bytes().to_vec());
+    packet.append(&mut raw_name.to_vec());
+    packet.append(&mut population.to_le_bytes().to_vec());
+    packet.append(&mut padding_suffix.to_vec());
+
+    println!("Raw packet: len={}, body={packet:?},", packet.len());
+
+    stream.write_all(&packet)?;
+    stream.flush()?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    // FIXME: Load host/port/configs from env vars
     let config = LoginServerConfig {
         host: "0.0.0.0".to_string(),
         port: 6900,
@@ -26,147 +161,50 @@ fn main() -> std::io::Result<()> {
 
     println!("Server up and running at {host_port}..");
 
-    // accept connections and process them serially
+    // FIXME: Attach all streams to a vector and iterate in a non-blocking way over them to support multiple connections
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
         stream.set_nodelay(true).unwrap();
         let mut stream = BufStream::new(&mut stream);
         println!("GOT A NEW CONNECTION!!");
 
-        let return_bad_password = false;
-
         loop {
-            // Read the packet type
+            // Determine the type of command being called by connected client
             let mut raw_command_type: [u8; 2] = [0; 2];
             let command_type_check = stream.read_exact(&mut raw_command_type);
+            if command_type_check.is_err() {
+                println!("Connection lost!");
+                break;
+            }
+            let command_type = LittleEndian::read_u16(&raw_command_type);
+            println!("COMMAND TYPE: 0x{command_type:04x}");
 
-            if command_type_check.is_ok() {
-                let command_type = LittleEndian::read_u16(&raw_command_type);
-                println!("COMMAND TYPE: 0x{command_type:04x}");
+            // FIXME: Make a function that maps command prefix to an enum or structs
 
-                if command_type == LOGIN_AUTH_ATTEMPT {
-                    println!("This is a login attempt command");
-                    // Padding, not sure what these represent yet
-                    let mut padding: [u8; 4] = [0; 4];
-                    stream.read_exact(&mut padding)?;
+            // Handle the command from the client
+            if command_type == LOGIN_AUTH_ATTEMPT {
+                println!("This is a login attempt command");
 
-                    let mut username = [0; 24];
-                    stream.read_exact(&mut username)?;
+                let auth_data = read_login_authenticate_data(&mut stream)?;
+                let username = auth_data.username;
+                let password = auth_data.password;
 
-                    let mut password = [0; 24];
-                    stream.read_exact(&mut password)?;
+                println!("LOGIN CREDENTIALS, username={username:?}, password={password:?}");
 
-                    println!("RAW LOGIN CREDENTIALS, username={username:?}, password={password:?}");
-
-                    let username_index_of_null =
-                        username.iter().enumerate().find(|(_index, c)| **c == 0);
-                    let username = String::from_utf8(
-                        username
-                            .to_vec()
-                            .iter()
-                            .take(match username_index_of_null {
-                                Some((index, _char)) => index,
-                                _ => username.len(),
-                            })
-                            .copied()
-                            .collect(),
-                    )
-                    .unwrap();
-
-                    let password_index_of_null =
-                        password.iter().enumerate().find(|(_index, c)| **c == 0);
-                    let password = String::from_utf8(
-                        password
-                            .to_vec()
-                            .iter()
-                            .take(match password_index_of_null {
-                                Some((index, _char)) => index,
-                                _ => password.len(),
-                            })
-                            .copied()
-                            .collect(),
-                    )
-                    .unwrap();
-
-                    // Gender? 0x00 for male, 0x01 for female?
-                    let mut gender: [u8; 1] = [0; 1];
-                    stream.read_exact(&mut gender)?;
-
-                    println!("LOGIN CREDENTIALS, username={username:?}, password={password:?}");
-
-                    // Fake an invalid login error
-                    if return_bad_password {
-                        println!("Returning invalid password!");
-                        let bad_password: [u8; 30] = [
-                            0xe0, 0x0a, 0x54, 0x14, 0x00, 0x00, 0x2b, 0x00, 0x00, 0x00, 0x00, 0x00,
-                            0x00, 0x00, 0x2d, 0x00, 0x00, 0x2d, 0x00, 0x00, 0x20, 0x00, 0x00, 0x3a,
-                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                        ];
-                        stream.write_all(&bad_password)?;
-                        stream.flush().unwrap();
-                        continue;
-                    } else {
-                        println!("Returning successful login..");
-                        let login_success_command: u16 = 0x0a4d;
-                        let weird_bytes: u16 = 0x00a0;
-                        let login_id1: u32 = 123456;
-                        let account_id: u32 = 55555;
-                        let login_id2: u32 = 0;
-                        let unknown_bytes: [u8; 31] = [0; 31];
-
-                        let mut raw_mnemonic: [u8; 16] = [0; 16];
-                        for (ch_index, ch) in "TPZMgc02COiARyrU".chars().enumerate() {
-                            raw_mnemonic[ch_index] = ch as u8;
-                        }
-                        let gender: u8 = 0;
-
-                        let mut packet: Vec<u8> = vec![];
-                        packet.append(&mut login_success_command.to_le_bytes().to_vec());
-                        packet.append(&mut weird_bytes.to_le_bytes().to_vec());
-                        packet.append(&mut login_id1.to_le_bytes().to_vec());
-                        packet.append(&mut account_id.to_le_bytes().to_vec());
-                        packet.append(&mut login_id2.to_le_bytes().to_vec());
-                        packet.append(&mut unknown_bytes.to_vec());
-                        packet.append(&mut raw_mnemonic.to_vec());
-                        packet.append(&mut gender.to_le_bytes().to_vec());
-                        println!("Raw packet: len={}, body={packet:?},", packet.len());
-
-                        stream.flush()?;
-
-                        println!("Returning a server list..");
-                        let server_list_command_prefix: u16 = 0xf180;
-                        let population: u16 = 123;
-                        let ip_suffix: &[u8; 2] = &[1, 70];
-                        let port: u16 = 4501;
-
-                        let name = "Cerberus";
-                        let mut raw_name: [u8; 20] = [0; 20];
-                        for (ch_index, ch) in name.chars().enumerate() {
-                            raw_name[ch_index] = ch as u8;
-                        }
-
-                        let padding_suffix: [u8; 4] = [0; 4];
-
-                        let mut packet: Vec<u8> = vec![];
-                        packet.append(&mut server_list_command_prefix.to_le_bytes().to_vec());
-                        packet.append(&mut ip_suffix.to_vec());
-                        packet.append(&mut port.to_le_bytes().to_vec());
-                        packet.append(&mut raw_name.to_vec());
-                        packet.append(&mut population.to_le_bytes().to_vec());
-                        packet.append(&mut padding_suffix.to_vec());
-                        println!("Raw packet: len={}, body={packet:?},", packet.len());
-                        stream.write_all(&packet)?;
-                        stream.write_all(&packet)?;
-                        stream.write_all(&packet)?;
-
-                        stream.flush()?;
-                    }
-                } else {
-                    println!("UNHANDLED PACKET TYPE: {:?}", command_type);
+                // Fake an invalid login error
+                if password != "asdfasdf" {
+                    println!("Returning invalid password!");
+                    write_login_invalid_login_error(&mut stream)?;
                     break;
                 }
+
+                println!("Returning successful login..");
+                write_login_authenticate_success(&mut stream)?;
+
+                println!("Returning a server list..");
+                write_login_character_servers_list(&mut stream)?;
             } else {
-                println!("Connection lost!");
+                println!("UNHANDLED PACKET TYPE: {:?}", command_type);
                 break;
             }
         }
